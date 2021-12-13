4a4f07942f20e1da8622bc0d54709781cd7cbc07
Kumaresan Lakshmanan <kaymatrix@gmail.com>
2021-12-13 13:28:23 +0530

    Update syncrepo.sh

--- scripts/syncrepo.sh
+++ scripts/syncrepo.sh
@@ -10,6 +10,8 @@ git config --global user.email "kaymatrix@gmail.com"
 git config --global user.name "kaymatrix"
 git add .
 git commit -m "Updated!"
-git push https://github.com/kaymatrix/actiontest2.git
+git push https://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@github.com/kaymatrix/actiontest2.git
+REM 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
+REM git push https://<GITHUB_ACCESS_TOKEN>@github.com/<GITHUB_USERNAME>/<REPOSITORY_NAME>.git
 
 echo "end"
